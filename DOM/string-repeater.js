f7ede87466f71a0fa59cc9da0fcf38cd93c0d7f4
RAFAEL LI CHEN <rafaellichen@gmail.com>
2017-10-28 11:14:39 -0400

    finished

---
+++ DOM/string-repeater.js
@@ -0,0 +1,9 @@
+var input1 = window.prompt("Type something")
+for(var i=0; i<3; i++) {
+    alert(input1)
+}
+var input2 = window.prompt("Type something again")
+var input3 = Number(window.prompt("Type a number"))
+for(var i=0; i<input3; i++) {
+    alert(input2)
+}
